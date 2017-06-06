3aa016003284bf4826289aa9109db20e87c09171
Unknown <adrienmontagu@gmail.com>
2017-06-06 17:48:19 +0200

    correct non identical grid

--- boxes.py
+++ boxes.py
@@ -9,29 +9,29 @@ class BoxesGame():
 		pygame.font.init()
 
 
-		self.lineSize = 7
-		self.columnSize = 6
+		self.lineSize = 6
+		self.columnSize = 8
 
 		self.squareSize = 64
 		self.separatorSize = 5
 
-		width = self.columnSize * self.squareSize + self.separatorSize
-		height = self.columnSize * self.squareSize + self.separatorSize + 100
+		self.width = self.columnSize* self.squareSize + self.separatorSize
+		self.height = self.lineSize * self.squareSize + self.separatorSize + 100
 		#2
 		#initialize the screen
-		self.screen = pygame.display.set_mode((width, height))
+		self.screen = pygame.display.set_mode((self.width, self.height))
 		pygame.display.set_caption("Boxes")
 		#3
 		#initialize pygame clock
 		self.clock=pygame.time.Clock()
-		self.boardh = [[False for x in range(self.columnSize)] for y in range(self.lineSize)]
-		self.boardv = [[False for x in range(self.lineSize)] for y in range(self.columnSize)]
+		self.boardh = [[False for x in range(self.columnSize)] for y in range(self.lineSize+1)]
+		self.boardv = [[False for x in range(self.columnSize+1)] for y in range(self.lineSize)]
 		self.initGraphics()
 		self.turn = True
 		self.me=0
 		self.ia=0
-		self.didiwin=False
-		self.owner = [[0 for x in range(6)] for y in range(6)]
+		self.userWin=False
+		self.owner = [[0 for x in range(self.lineSize)] for y in range(self.columnSize)]
 
 	def initGraphics(self):
 		self.normallinev=pygame.image.load("images/normalline.png")
@@ -50,26 +50,26 @@ class BoxesGame():
 		self.score_panel=pygame.image.load("images/score_panel.png")
 
 	def drawBoard(self):
-		for x in range(6):
-			for y in range(7):
+		for x in range(self.columnSize):
+			for y in range(self.lineSize+1):
 				if not self.boardh[y][x]:
 					self.screen.blit(self.normallineh, [(x)*self.squareSize+self.separatorSize, (y)*self.squareSize])
 				else:
 					self.screen.blit(self.bar_doneh, [(x)*self.squareSize+self.separatorSize, (y)*self.squareSize])
-		for x in range(7):
-			for y in range(6):
+		for x in range(self.columnSize+1):
+			for y in range(self.lineSize):
 				if not self.boardv[y][x]:
 					self.screen.blit(self.normallinev, [(x)*self.squareSize, (y)*self.squareSize+self.separatorSize])
 				else:
 					self.screen.blit(self.bar_donev, [(x)*self.squareSize, (y)*self.squareSize+self.separatorSize])
 		#draw separators
-		for x in range(7):
-			for y in range(7):
+		for x in range(self.columnSize+1):
+			for y in range(self.lineSize+1):
 				self.screen.blit(self.separators, [x*self.squareSize, y*self.squareSize])
 
 	def drawOwnermap(self):
-		for x in range(6):
-			for y in range(6):
+		for x in range(self.columnSize):
+			for y in range(self.lineSize):
 				if self.owner[x][y]!=0:
 					if self.owner[x][y]=="win":
 						self.screen.blit(self.blueplayer, (x*self.squareSize+self.separatorSize, y*self.squareSize+self.separatorSize))
@@ -78,7 +78,7 @@ class BoxesGame():
 
 	def drawHUD(self):
 		#draw the background for the bottom:
-		self.screen.blit(self.score_panel, [0, 389])
+		self.screen.blit(self.score_panel, [0, self.height - 100])
 
 		#create font
 		myfont = pygame.font.SysFont(None, 32)
@@ -87,8 +87,8 @@ class BoxesGame():
 		label = myfont.render("Your Turn:", 1, (255,255,255))
 
 		#draw surface
-		self.screen.blit(label, (10, 400))
-		self.screen.blit(self.greenindicator, (130, 395))
+		self.screen.blit(label, (10, self.height - 90))
+		self.screen.blit(self.greenindicator, (130, self.height - 95))
 		#same thing here
 		myfont64 = pygame.font.SysFont(None, 64)
 		myfont20 = pygame.font.SysFont(None, 20)
@@ -98,13 +98,13 @@ class BoxesGame():
 		scoretextme = myfont20.render("You", 1, (255,255,255))
 		scoretextother = myfont20.render("IA", 1, (255,255,255))
 
-		self.screen.blit(scoretextme, (10, 425))
-		self.screen.blit(scoreme, (10, 435))
-		self.screen.blit(scoretextother, (280, 425))
-		self.screen.blit(scoreother, (340, 435))
+		self.screen.blit(scoretextme, (10, self.height - 60))
+		self.screen.blit(scoreme, (10, self.height - 50))
+		self.screen.blit(scoretextother, (280, self.height - 60))
+		self.screen.blit(scoreother, (340, self.height - 50))
 
 	def checkCloseSquare(self, xpos, ypos, isHorizontal, isUser):
-		# print("checkCloseSquare xpos = ",xpos, " , ypos = ", ypos)
+		print("checkCloseSquare xpos = ",xpos, " , ypos = ", ypos)
 		"""print(self.boardv)
 		print(self.boardh)
 		print(self.boardh[ypos][xpos])
@@ -112,15 +112,25 @@ class BoxesGame():
 		print(self.boardh[ypos-1][xpos])"""
 
 		if isHorizontal:
-			if self.boardh[ypos][xpos] == True and self.boardh[ypos + 1][xpos] == True and self.boardv[ypos][xpos] == True and self.boardv[ypos][xpos+1] == True:
-				self.fillSquare(xpos, ypos, isUser)
-			if self.boardh[ypos][xpos] == True and self.boardh[ypos - 1][xpos] == True and self.boardv[ypos-1][xpos] == True and self.boardv[ypos-1][xpos+1] == True:
-				self.fillSquare(xpos, ypos-1, isUser)
+			if ypos < self.lineSize and xpos < self.columnSize:
+				if self.boardh[ypos][xpos] == True and self.boardh[ypos + 1][xpos] == True and self.boardv[ypos][xpos] == True and self.boardv[ypos][xpos+1] == True:
+					self.fillSquare(xpos, ypos, isUser)
+			else:
+				print("ici")
+
+			if ypos > 0 and xpos < self.columnSize:
+				if self.boardh[ypos][xpos] == True and self.boardh[ypos - 1][xpos] == True and self.boardv[ypos-1][xpos] == True and self.boardv[ypos-1][xpos+1] == True:
+					self.fillSquare(xpos, ypos-1, isUser)
+			else:
+				print("ici2")
 		else:
-			if self.boardv[ypos][xpos] == True and self.boardv[ypos][xpos+1] == True and self.boardh[ypos][xpos] == True and self.boardh[ypos][xpos] and self.boardh[ypos+1][xpos]:
-				self.fillSquare(xpos, ypos, isUser)
-			if self.boardv[ypos][xpos] == True and self.boardv[ypos][xpos-1] == True and self.boardh[ypos][xpos-1] == True and self.boardh[ypos+1][xpos-1]:
-				self.fillSquare(xpos-1, ypos, isUser)
+			if ypos < self.lineSize and xpos < self.columnSize:
+				if self.boardv[ypos][xpos] == True and self.boardv[ypos][xpos+1] == True and self.boardh[ypos][xpos] == True and self.boardh[ypos][xpos] and self.boardh[ypos+1][xpos]:
+					self.fillSquare(xpos, ypos, isUser)
+
+			if ypos < self.lineSize and xpos > 0:
+				if self.boardv[ypos][xpos] == True and self.boardv[ypos][xpos-1] == True and self.boardh[ypos][xpos-1] == True and self.boardh[ypos+1][xpos-1]:
+					self.fillSquare(xpos-1, ypos, isUser)
 
 
 
@@ -134,6 +144,11 @@ class BoxesGame():
 
 	def update(self):
 		#sleep to make the game 60 fps
+
+		if self.me + self.ia == self.lineSize * self.columnSize:
+			self.userWin = True if self.me > self.ia else False
+			return 1
+
 		self.clock.tick(60)
 
 		for event in pygame.event.get():
@@ -207,7 +222,7 @@ class BoxesGame():
 		pygame.display.flip()
 
 	def finished(self):
-		self.screen.blit(self.gameover if not self.didiwin else self.winningscreen, (0,0))
+		self.screen.blit(self.gameover if not self.userWin else self.winningscreen, (0,0))
 		while 1:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
@@ -217,4 +232,6 @@ class BoxesGame():
 if __name__ == "__main__":
 	bg=BoxesGame() #__init__ is called right here
 	while 1:
-		bg.update()
+		if bg.update() == 1:
+			break
+	bg.finished()
